7831e33d2eaf9206a353775df51dc9ae8a12a932
Dillon Reedy <dillon.reedy123@gmail.com>
2021-02-21 18:04:33 -0600

    Changing title again

--- src/stories/Header.js
+++ src/stories/Header.js
@@ -24,7 +24,7 @@ export const Header = ({ user, onLogin, onLogout, onCreateAccount }) => (
             />
           </g>
         </svg>
-        <h1>Acme</h1>
+        <h1>Another file change</h1>
       </div>
       <div>
         {user ? (
