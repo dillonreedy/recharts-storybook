8b40fbc99b3e8d6ccb96b2b38cfb953b6edd2716
Dillon Reedy <dillon.reedy123@gmail.com>
2021-02-27 00:21:27 -0600

    Removed docs panel

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,4 +2,9 @@
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: { expanded: true },
+  previewTabs: {
+    'storybook/docs/panel': {
+      hidden: true
+    }
+  }
 }
