a2a31bcf81ef3e91342343d263abc2d97d1f5679
Dillon Reedy <dillon.reedy123@gmail.com>
2021-03-03 00:15:55 -0600

    Got rid of storysource and jsx decorator for now.

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports = {
   "stories": [
     "../src/**/*.stories.mdx",
@@ -5,7 +7,6 @@ module.exports = {
   ],
   "addons": [
     "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    '@storybook/addon-jsx'
+    "@storybook/addon-essentials"  
   ]
 }
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,13 +1,6 @@
 const { addDecorator } = require('@storybook/react');
-const { jsxDecorator } = require('storybook-addon-jsx');
 
-addDecorator(jsxDecorator);
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: { expanded: true },
-  previewTabs: {
-    'storybook/docs/panel': {
-      hidden: true
-    }
-  }
 }
