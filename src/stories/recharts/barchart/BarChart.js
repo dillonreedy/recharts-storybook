67aa2432172934b16ec0c24af6c7a15b9a76c1b7
Dillon Reedy <dillon.reedy123@gmail.com>
2021-12-11 19:59:47 -0600

    Added basic bar chart

---
+++ src/stories/recharts/barchart/BarChart.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import { BarChart as RechartsBarChart, CartesianGrid, XAxis, YAxis, Tooltip, Legend, Bar } from 'recharts';
+import PropTypes from 'prop-types';
+
+export const BarChart = (({dataKey, pvStroke, uvStroke, ...props}) => {
+  const width = 770;
+  const height = 250;
+  const data = [
+    {
+      name: 'Page A',
+      uv: 4000,
+      pv: 2400,
+      amt: 2400,
+    },
+    {
+      name: 'Page B',
+      uv: 3000,
+      pv: 1398,
+      amt: 2210,
+    },
+    {
+      name: 'Page C',
+      uv: 2000,
+      pv: 9800,
+      amt: 2290,
+    },
+    {
+      name: 'Page D',
+      uv: 2780,
+      pv: 3908,
+      amt: 2000,
+    },
+    {
+      name: 'Page E',
+      uv: 1890,
+      pv: 4800,
+      amt: 2181,
+    },
+    {
+      name: 'Page F',
+      uv: 2390,
+      pv: 3800,
+      amt: 2500,
+    },
+    {
+      name: 'Page G',
+      uv: 3490,
+      pv: 4300,
+      amt: 2100,
+    },
+  ];
+  const margin = { top: 5, right: 30, left: 20, fabottom: 5 };
+
+  
+  return (
+    <RechartsBarChart
+    width={width}
+    height={height}
+    data={data}
+    margin={margin}
+  >
+    <CartesianGrid strokeDasharray="3 3" />
+    <XAxis dataKey={dataKey} />
+    <YAxis />
+    <Tooltip />
+    <Legend />
+    <Bar dataKey="pv" fill={pvStroke} />
+    <Bar dataKey="uv" fill={uvStroke} />
+  </RechartsBarChart>
+  )
+});
+
+BarChart.propTypes = {
+   /**
+   * The color of the pv bar.
+   */
+  pvStroke: PropTypes.string,
+    /**
+   * The color of the uv bar.
+   */
+  uvStroke: PropTypes.string
+}
+
+BarChart.defaultProps = {
+    pvStroke: '#8884d8',
+    uvStroke: '#82ca9d'
+}
---
+++ src/stories/recharts/barchart/BarChart.stories.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BarChart } from './BarChart';
+
+export default {
+  title: 'Recharts/cartesian/BarChart',
+  component: BarChart,
+  argTypes: {
+    pvStroke: {
+        control: 'color'
+    },
+    uvStroke: {
+        control: 'color'
+    },
+  },
+};
+
+const Template = (args) => <BarChart {...args} />;
+
+export const Simple = Template.bind({});
+Simple.args = {
+  dataKey: 'name',
+  pvStroke: '#8884d8',
+  uvStroke: '#82ca9d'
+};
