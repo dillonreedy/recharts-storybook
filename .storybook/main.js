7437938898836090c8f25a180f7785e526584b64
Dillon Reedy <dillon.reedy123@gmail.com>
2021-12-17 10:12:04 -0600

    storybook updates

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,11 @@
-const path = require('path');
-
 module.exports = {
   "stories": [
     "../src/**/*.stories.mdx",
-    "../src/**/**/*.stories.@(js|jsx|ts|tsx)",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
   "addons": [
     "@storybook/addon-links",
-    "@storybook/addon-essentials"  
-  ]
+    "@storybook/addon-essentials"
+  ],
+  "framework": "@storybook/react"
 }
