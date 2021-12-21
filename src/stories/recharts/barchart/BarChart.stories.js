fe7afae55b6cace98fb9a629afff9d5d7c976a49
Dillon Reedy <dillon.reedy123@gmail.com>
2021-12-20 19:28:49 -0600

    Brokedown barcharts props into two stories

--- src/stories/recharts/barchart/BarChart.stories.js
+++ src/stories/recharts/barchart/BarChart.stories.js
@@ -6,12 +6,16 @@ const subcomponentArgs = BarChartService.getSubcomponentArgs();
 const args = combine(barChartArgs, subcomponentArgs);
 
 export default {
-  title: 'Recharts/Bar Chart',
+  title: 'Recharts/Bar Chart/Props',
   argTypes: BarChartService.getArgTypes()
 };
 
 const Template = BarChartService.getTemplate();
 
-export const Controls = Template.bind({});
-Controls.parameters = { controls: {exclude: Object.keys(subcomponentArgs)}}
-Controls.args = args;
+export const Sizing = Template.bind({});
+Sizing.parameters = { controls: {include: ['width','height']}}
+Sizing.args = args;
+
+export const Data = Template.bind({});
+Data.parameters = { controls: {include: 'data'}}
+Data.args = args;
